2b361e5b67c46aab07abe3c1816b53bd1b43677a
Hassan Cissey <hcissey0@gmail.com>
2024-01-18 20:10:51 -0800

    glam

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
 -- Lists all bands with glam rock as their style
-SELECT band_name, COALESCE(2022 - formed - 2, 0) AS lifespan
+SELECT band_name, COALESCE(split - formed, 2022 - formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-AND split IS NULL
 ORDER BY lifespan DESC;
