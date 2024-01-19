545dac40af8b3d875576a34e0904b0939b73c321
Hassan Cissey <hcissey0@gmail.com>
2024-01-18 19:08:13 -0800

    fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- This ranks the origin of bands ordered by nnumbers
-SELECT origin, SUM(nb_fans) AS nfans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY nfans DESC;
+ORDER BY nb_fans DESC;
