d4940680d92a6dbcfbf3d659f6db7c67cf2f3941
Hassan Cissey <hcissey0@gmail.com>
2024-01-24 20:15:50 -0800

    reading from redis and return the original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Union
+import typing
 
 
 class Cache:
@@ -14,9 +14,28 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: typing.Union[str, bytes, int, float]) -> str:
         """This is going to create a random key
         and save it nd return the key"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: typing.Optional[typing.Callable] = None) -> typing.Union[
+                    str, bytes, int, float]:
+        """Thisi s the get function to get a key"""
+        val = self._redis.get(key)
+        if fn is not None and val is not None:
+            return fn(val)
+        return val
+
+    def get_str(self, key: str) -> str:
+        """this function is used to get strings"""
+        val = self.get(key, fn=lambda x: x.decode())
+        return val
+
+    def get_int(self, key: str) -> int:
+        """This function is used to get integers"""
+        val = self.get(key, fn=int)
+        return val
