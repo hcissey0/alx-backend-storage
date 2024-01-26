42d8e1475cfdbd26799099e47e5f5ef84de9b24b
Hassan Cissey <hcissey0@gmail.com>
2024-01-25 22:16:57 -0800

    redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -10,15 +10,6 @@ from functools import wraps
 red = redis.Redis()
 
 
-def count_call(method: typing.Callable) -> typing.Callable:
-    """This is used to count the number of funcion calls"""
-    @wraps(method)
-    def wrapp(*args, **kwargs):
-        red.incr(f"count:{args[0]}")
-        return method(*args)
-    return wrapp
-
-
 def cache_page(method: typing.Callable) -> typing.Callable:
     """Thisis used to cache th apge"""
     @wraps(method)
@@ -28,13 +19,14 @@ def cache_page(method: typing.Callable) -> typing.Callable:
             return page.decode('utf-8')
         else:
             page = method(*args)
-            red.setex(args[0], 10, page)
+            red.set(args[0], page, ex=10)
             return page
     return wrapper
 
 
-@count_call
 @cache_page
 def get_page(url: str) -> str:
     """Thisisi the function that gets the page"""
-    return requests.get(url).text
+    res = requests.get(url).text
+    red.incr(f'count:{url}')
+    return res
